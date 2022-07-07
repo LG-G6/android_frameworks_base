7fa6ff7a4d7ed0791014375b639d128b17e8925b
Bernardo Bas <31733216+BernardoBas@users.noreply.github.com>
2022-07-07 17:18:55 -0300

    PixelPropsUtils: Update fingerprint to July 2022

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -67,7 +67,7 @@ public class PixelPropsUtils {
         propsToChange.put("DEVICE", "raven");
         propsToChange.put("PRODUCT", "raven");
         propsToChange.put("MODEL", "Pixel 6 Pro");
-        propsToChange.put("FINGERPRINT", "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys");
+        propsToChange.put("FINGERPRINT", "google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys");
         propsToChangePixelXL = new HashMap<>();
         propsToChangePixelXL.put("BRAND", "google");
         propsToChangePixelXL.put("MANUFACTURER", "Google");
